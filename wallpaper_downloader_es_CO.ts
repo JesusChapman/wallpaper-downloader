da9560aacbade9ffdba7fc1c5661f9af31d1e461
JesusChapman <jesuschapmandev@outlook.com>
2025-11-22 10:29:27 -0500

    add app preview screenshot

--- wallpaper_downloader_es_CO.ts
+++ wallpaper_downloader_es_CO.ts
@@ -119,7 +119,7 @@
         <location filename="mainwindow.ui" line="258"/>
         <location filename="build/Desktop_Qt_6_10_0_MSVC2022_64bit-Debug/wallpaper_downloader_autogen/include/ui_mainwindow.h" line="216"/>
         <location filename="build/Desktop_Qt_6_10_0_MSVC2022_64bit-MinSizeRel/wallpaper_downloader_autogen/include/ui_mainwindow.h" line="212"/>
-        <location filename="mainwindow.cpp" line="394"/>
+        <location filename="mainwindow.cpp" line="395"/>
         <source>About Qt</source>
         <translation>Acerca de Qt</translation>
     </message>
@@ -127,114 +127,114 @@
         <location filename="mainwindow.ui" line="263"/>
         <location filename="build/Desktop_Qt_6_10_0_MSVC2022_64bit-Debug/wallpaper_downloader_autogen/include/ui_mainwindow.h" line="217"/>
         <location filename="build/Desktop_Qt_6_10_0_MSVC2022_64bit-MinSizeRel/wallpaper_downloader_autogen/include/ui_mainwindow.h" line="213"/>
-        <location filename="mainwindow.cpp" line="399"/>
+        <location filename="mainwindow.cpp" line="400"/>
         <source>About Wallpaper downloader</source>
         <translation>Acerca de Descargador de fondos de pantalla</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="333"/>
+        <location filename="mainwindow.cpp" line="334"/>
         <source>Application Settings</source>
         <translation>Ajustes de aplicación</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="338"/>
+        <location filename="mainwindow.cpp" line="339"/>
         <source>Application Theme:</source>
         <translation>Tema de la aplicación:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="340"/>
+        <location filename="mainwindow.cpp" line="341"/>
         <source>Follow System</source>
         <translation>Seguir Sistema</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="341"/>
+        <location filename="mainwindow.cpp" line="342"/>
         <source>Light</source>
         <translation>Claro</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="342"/>
+        <location filename="mainwindow.cpp" line="343"/>
         <source>Dark</source>
         <translation>Oscuro</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="346"/>
+        <location filename="mainwindow.cpp" line="347"/>
         <source>Background Effect (Requires partial restart):</source>
         <translation>Efecto de Fondo (Requiere Reinicio parcial):</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="348"/>
+        <location filename="mainwindow.cpp" line="349"/>
         <source>Mica (Opaque, Performance)</source>
         <translation>Mica (Opaco, Rendimiento)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="349"/>
+        <location filename="mainwindow.cpp" line="350"/>
         <source>Acrylic (Translucent, Blur)</source>
         <translation>Acrílico (Translúcido, Desenfoque)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="353"/>
+        <location filename="mainwindow.cpp" line="354"/>
         <source>Destination Folder:</source>
         <translation>Carpeta de destino:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="366"/>
+        <location filename="mainwindow.cpp" line="367"/>
         <source>Select Folder</source>
         <translation>Seleccionar Carpeta</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="383"/>
+        <location filename="mainwindow.cpp" line="384"/>
         <source>Settings updated</source>
         <translation>Ajustes actualizados</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="400"/>
+        <location filename="mainwindow.cpp" line="401"/>
         <source>&lt;h3&gt;Wallpaper downloader v0.9-dev-preview&lt;/h3&gt;&lt;p&gt;Created by: Jesus Chapman&lt;br&gt;A simple app for download wallpapers using wallhaven api and Qt Tecnologies&lt;/p&gt;</source>
         <translation>&lt;h3&gt;Descargador de fondos de pantalla v0.9-dev-preview&lt;/h3&gt;&lt;p&gt;Creado por: Jesus Chapman&lt;br&gt;Una simple app para descargar fondos de pantalla usando la api de Wallhaven y tecnologías Qt&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="447"/>
+        <location filename="mainwindow.cpp" line="448"/>
         <source>Info</source>
         <translation>Info</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="447"/>
+        <location filename="mainwindow.cpp" line="448"/>
         <source>No results found.</source>
         <translation>No se encontraron resultados.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="491"/>
+        <location filename="mainwindow.cpp" line="492"/>
         <source>Starting download: </source>
         <translation>Iniciando descarga: </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="524"/>
+        <location filename="mainwindow.cpp" line="525"/>
         <source>Saved: </source>
         <translation>Guardado: </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="512"/>
-        <location filename="mainwindow.cpp" line="527"/>
+        <location filename="mainwindow.cpp" line="513"/>
+        <location filename="mainwindow.cpp" line="528"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="512"/>
-        <location filename="mainwindow.cpp" line="527"/>
+        <location filename="mainwindow.cpp" line="513"/>
+        <location filename="mainwindow.cpp" line="528"/>
         <source>Download failed.</source>
         <translation>Falló la descarga.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="534"/>
+        <location filename="mainwindow.cpp" line="535"/>
         <source>Preview</source>
         <translation>Vista Previa</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="546"/>
+        <location filename="mainwindow.cpp" line="547"/>
         <source>Open Folder</source>
         <translation>Abrir Carpeta</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="547"/>
+        <location filename="mainwindow.cpp" line="548"/>
         <source>Close</source>
         <translation>Cerrar</translation>
     </message>
